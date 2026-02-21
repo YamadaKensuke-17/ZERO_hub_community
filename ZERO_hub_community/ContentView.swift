ce9dbe41ea23004d38d572df2c84df79dc80dd42
山田健輔 <yk20050731@icloud.com>
2026-02-22 00:28:27 +0900

    Initial Commit

---
+++ ZERO_hub_community/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZERO_hub_community
+//
+//  Created by 山田健輔 on 2026/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZERO_hub_community/ZERO_hub_communityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZERO_hub_communityApp.swift
+//  ZERO_hub_community
+//
+//  Created by 山田健輔 on 2026/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ZERO_hub_communityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
